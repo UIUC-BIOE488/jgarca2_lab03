f326e4f9b9030511e1560989742d742a5c9b2ec3
Jhersin Garcia <jgarca2@illinois.com>
2024-09-27 22:57:29 -0500

    Lab 3 Jgarcia

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 from memory_profiler import profile
 import time
 
-fp = open('./memory_profiler.log', 'w+')
+fp = open('./memory_profiler.log', 'a+')
 @profile(stream=fp)
 def my_func():
     a = [1] * (5 * 10 ** 7)
@@ -12,21 +12,55 @@ def my_func():
     time.sleep(1)
     a = 2
     for i in range(30):
-        a = a*a
+        a = a * a
     time.sleep(1)
     del a
     return
 
-
-# [Exercise]
-# fp2 = open('./student.log', 'w+')
-# @profile(stream=fp2)
-# def student_func():
-#     return
-
-
-
 if __name__ == '__main__':
     my_func()
 
-#     student_func()
+
+
+
+
+
+
+
+
+
+
+
+
+# from memory_profiler import profile
+# import time
+#
+# fp = open('./memory_profiler.log', 'w+')
+# @profile(stream=fp)
+# def my_func():
+#     a = [1] * (5 * 10 ** 7)
+#     b = [2] * (2 * 10 ** 7)
+#     del b
+#     time.sleep(1)
+#     del a
+#     time.sleep(1)
+#     a = 2
+#     for i in range(30):
+#         a = a*a
+#     time.sleep(1)
+#     del a
+#     return
+#
+#
+# # [Exercise]
+# # fp2 = open('./student.log', 'w+')
+# # @profile(stream=fp2)
+# # def student_func():
+# #     return
+#
+#
+#
+# if __name__ == '__main__':
+#     my_func()
+#
+# #     student_func()
